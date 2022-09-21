75eb13b931c99807abd6e9b17079328702774a67
Abdullahi Elmi <abdullahielmi147@gmail.com>
2022-09-20 19:12:02 -0700

    got the displayPlayerHP working

--- app.js
+++ app.js
@@ -7,13 +7,17 @@ const resultSection = document.getElementById('result-display');
 const addGoblinForm = document.getElementById('add-goblin-form');
 const playerHP = document.getElementById('player-hp');
 
+const goblinList = document.getElementById('goblin-list');
+const removeDeadGoblinsBtn = document.getElementById('remove-dead-goblins');
+
 /* State */
 let message = '';
+let player = { HP: 16 };
 
 let goblins = [
-    { name: Lex, HP: 7 },
-    { name: Hamza, HP: 8 },
-    { name: Tech, HP: 9 },
+    { name: 'Lex', HP: 7 },
+    { name: 'Hamza', HP: 8 },
+    { name: 'Tech', HP: 9 },
 ];
 
 /* Events */
@@ -23,7 +27,9 @@ function displayGoblinHP() {}
 function displayMessage() {
     // took 3 damage and defeat
 }
-function displayPlayerHP() {}
+function displayPlayerHP() {
+    playerHP.textContent = player.HP;
+}
 displayMessage();
 displayGoblinHP();
 displayPlayerHP();
